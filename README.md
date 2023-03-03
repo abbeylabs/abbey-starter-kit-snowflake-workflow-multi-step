e2bd507c01bbf580a6babf020b8b3b91fa528222
Jeff Chao <jeffchao@me.com>
2023-03-03 12:24:21 -0800

    Add Snowflake workflow multi-step example.

---
+++ README.md
@@ -0,0 +1,53 @@
+# Workflow multi step example
+
+This example shows how to create a multistep Grant Kit workflow.
+The example features requesting access to a [Snowflake Role Grant](https://registry.terraform.io/providers/Snowflake-Labs/snowflake/latest/docs/resources/role_grants) 
+in two sequential steps, with the first step requiring all reviewers to approve access and 
+the second step requiring just one reviewer to approve access.
+
+## Prerequisites
+
+1. Abbey Labs Account - [Sign up for the waitlist](https://abbey.so).
+2. Snowflake Account
+
+## Usage
+
+1. Initialize the Terraform configuration.
+
+```shell
+terraform init
+```
+
+2. Create a tfvars file and add in your Snowflake variables.
+
+```shell
+echo 'account = "..."\nusername = "..."\npassword = "..."' > dev.tfvars
+```
+
+3. Replace the configuration stubs (denoted by `...` strings) in [main.tf](main.tf).
+4. Plan and apply your configuration
+
+```shell
+terraform plan
+terraform apply
+```
+
+## Automating Access Requests
+
+Once you've applied your configuration, you can automate your access requests through Abbey with the following steps:
+
+1. Copy the provided GitHub Actions Workflow template.
+
+```shell
+cp .github/workflows/abbey-grant-kit-generate-policy-input.yaml.example .github/workflows/abbey-grant-kit-generate-policy-input.yaml
+```
+
+2. Add your `SNOWFLAKE_USERNAME` and `SNOWFLAKE_PASSWORD` to your [GitHub Repository Secrets](https://docs.github.com/en/actions/security-guides/encrypted-secrets).
+3. :tada: Congratulations! Your access requests are now automated! You can tell your users to head over
+   to the [Abbey App](https://app.abbey.so) to start requesting access!
+
+## :books: Learn More
+
+To learn more about Grant Kits and Grant Workflows, visit the following resources:
+
+- [Abbey Labs Documentation](https://docs.abbey.so) - learn about automating access management with Abbey Labs.
---
+++ main.tf
@@ -0,0 +1,109 @@
+terraform {
+  required_providers {
+    abbey = {
+      source = "abbeylabs/abbey"
+      version = "0.1.0-rc.1"
+    }
+
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "0.56.5"
+    }
+  }
+}
+
+provider "abbey" {
+  # Configuration options
+}
+
+provider "snowflake" {
+  account   = var.account
+  username  = var.username
+  password  = var.password
+}
+
+# This example shows how you might manage Snowflake databases within Terraform.
+# Notice that we're using `data` here which implies creation of databases
+# and their schemas already happened out-of-band.
+#
+# You can provision databases and schemas in Terraform, but you may want
+# to consider using native Snowflake or database-friendly tooling to manage
+# schemas. Terraform and the Abbey Provider on the other hand would be great
+# for managing access.
+#
+# Make sure to put in valid values as they exist in Snowflake, otherwise you will
+# get Null data references when trying to `plan` or `apply`.
+data "snowflake_database" "pii_database" {
+  name = "..."
+}
+
+# This example shows how you might manage Snowflake roles within Terraform.
+# Notice that we're using `data` here which implies creation of the role
+# already happened out-of-band, for example, within the Snowflake console.
+#
+# If you would like to configure roles directly within Terraform, you can
+# visit the open source Snowflake-Labs/snowflake provider documentation.
+#
+# Make sure to put in valid values as they exist in Snowflake, otherwise you will
+# get Null data references when trying to `plan` or `apply`.
+data "snowflake_role" "pii_readonly_role" {
+  name = "..."
+}
+
+# This example shows how you might manage Snowflake identities within Terraform.
+# If you don't manage identities within Terraform, you can exclude this block.
+data "snowflake_users" "my_snowflake_user" {
+  pattern = "..."
+}
+
+# Provision an example table grant. Feel free to replace with your own table grant.
+# Before running, replace the stubbed fields in this block.
+resource "snowflake_table_grant" "pii_readonly__can_read__pii__table" {
+  database_name     = data.snowflake_database.pii_database.name
+  schema_name       = "..."
+  table_name        = "..."
+  privilege         = "SELECT"
+  roles             = [data.snowflake_role.pii_readonly_role.name]
+  with_grant_option = false
+}
+
+resource "abbey_grant_kit" "role__pii_readonly" {
+  name = "PII READONLY role grant"
+  description = <<-EOT
+    Grants access to the PII READONLY Snowflake Role Grant.
+    This Grant Kit uses a multi-step Grant Workflow that has two sequential steps,
+    with the first step requiring all reviewers to approve access and
+    the second step requiring just one reviewer to approve access.
+  EOT
+
+  workflow = {
+    steps = [
+      {
+        reviewers = {
+          # Replace with your Primary Identities.
+          # For more information on what a Primary Identity is, visit https://docs.abbey.so.
+          all_of = ["...", "...", "..."]
+        }
+      },
+      {
+        reviewers = {
+          # Replace with your Primary Identity.
+          # For more information on what a Primary Identity is, visit https://docs.abbey.so.
+          one_of = ["..."]
+        }
+      }
+    ]
+  }
+
+  output = {
+    # Replace with your own path pointing to where you want your access changes to manifest.
+    # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
+    location = "github://abbeylabs/abbey-starter-kits/snowflake/workflow-multi-step/access.tf"
+    append = <<-EOT
+      resource "snowflake_role_grants" "pii_readonly__{{ $.data.system.abbey.primary_identity.username }}" {
+        role_name = "${data.snowflake_role.pii_readonly_role.name}"
+        users     = ["{{ $.data.system.abbey.secondary_identities.snowflake.username }}"]
+      }
+    EOT
+  }
+}
